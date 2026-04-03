7646994177305897ef1be6ae4f011ad02e69fb26
Arthur Sacramento <sacrarthur06@gmail.com>
2026-04-03 14:16:30 -0300

    Implementação de Insertion e Merge

---
+++ main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+
+void insertionSort (int *v, int left, int right){
+
+    int i = left +1;
+    int j,aux;
+
+    for(i; i <= right ; i++){
+        aux = v[i];
+        for(j = i; (j > left)&& (aux < v[j-1]); j--){
+            v[j] = v[j-1];
+        }
+        v[j] = aux;
+    }
+}
+
+void mergeSort (int *v, int start, int mid, int end){
+
+    int n1 = mid - start +1;
+    int n2 = end - mid;
+    int left [n1];
+    int right [n2];
+    int i, j;
+    int k = start;
+
+    for(i = 0; i<n1; i++){
+        left[i] = v [start + i];
+     }
+
+    for(j=0; j< n2; j++){
+        right[j] = v[mid +1 +j];
+    }
+
+    i =0;
+    j = 0;
+
+    while(i < n1 && j < n2){
+
+        if(left[i] < right[j]){
+            v[k] = left [i];
+            i++;
+        }else{
+            v[k] = right [j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i<n1){
+        v[k] = left[i];
+        i++;
+        k++;
+    }
+
+    while(j<n2){
+        v[k] = right[j];
+        j++;
+        k++;
+    }
+
+}
+
+
+int main(){
+
+
+
+}
